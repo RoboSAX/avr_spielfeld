a5d6bc8dc306c8895f5a158ca1d762a5143104e1
Leander Herr <droelf@partybombe.de>
2019-11-16 23:19:10 +0100

    make task 2020 ready

--- RoboSAX/2020/Spielfeld/src/gamemodes.c
+++ RoboSAX/2020/Spielfeld/src/gamemodes.c
@@ -36,15 +36,57 @@
 #define BLINKCOUNTDOWNMAX 3
 #define BLINKCOUNTMULTI 6
 
+#define MAX_TRYS 5
+#define POINTS_PER_PRESS 5
+
 //**************************<Types and Variables>******************************
 enum eGamemodes {
 	gm1P	 = 0,
 	gm2P		,
 	MaxGameModes,
 };
+struct sBlink{
+	uint8_t LEDNr : 3;
+	uint8_t countdown : 2;
+	uint8_t numberOfBlinks : 3;
+};
+enum eGroup {
+	groupOff = 0,
+	group1 = 1,
+	group2 = 2,
+};
+struct sTeamGroup{
+	enum eGroup status;
+	uint8_t firstLED;
+};
+struct sTeam {
+	uint8_t points;
+	uint8_t trys;
+	struct sTeamGroup groups[3];
+	uint8_t offGroup;
+
+	enum eColor teamColor;
+};
+
+volatile struct sTeam team[2];
+
+enum eTeamNumber {
+	team1,
+	team2
+};
+
+struct sGlobalLED{
+	uint8_t teamNr : 2;
+	uint8_t GroupNr : 3;
+	uint8_t LEDNr : 3;
+};
+struct sGlobalLED LEDToTeam[LEDBOX_COUNT_MAX];
 
 enum eGamemodes usedGamemode;
 //**************************<Methods>******************************************
+void pushButton(uint8_t number);
+void setLEDs(void);
+
 void gamemode_init(void){
 	maxGameModes=MaxGameModes;
 	random_init();
@@ -60,8 +102,48 @@ uint8_t gamemode_start(uint8_t gameMode){
 
 		usedGamemode=gameMode;
 
-		//init Led to team with respect to field size
+		team[team1].teamColor = TEAM1COLOR;
+		team[team1].offGroup = 3;
+		team[team1].trys = 0;
+		team[team1].points = 0;
 
+		team[team2].teamColor = TEAM2COLOR;
+		team[team2].offGroup = 3;
+		team[team2].trys = 0;
+		team[team2].points = 0;
+
+    uint16_t randomNumber= random();
+		uint8_t  teamNr;
+		for (teamNr=0;teamNr<2;teamNr++){
+			uint8_t i;
+			for(i=0;i<3;i++){
+				team[teamNr].groups[i].status = randomNumber % 2 + 1;
+				randomNumber /= 2;
+			}
+		}
+		//init Led to team with respect to field size
+		uint8_t i;
+		for(i=0;i< ledbox_count_current / 4;i++){
+			const uint8_t teamNr = ((ledbox_state == half_field) || (i < 1) || (i > 3)) ? 0 : 1;
+			const uint8_t groupNr = ((ledbox_state == half_field) || (i < 1)) ? i : (i < 4) ? i - 1 : i - 3;
+			uint8_t j;
+			for(j=0;j< 4;j++){
+				uint8_t number = i * 4 + j;
+				switch(j){
+					case 0:
+					case 3:
+								LEDToTeam[number].teamNr = 2;
+								break;
+					case 1:
+								team[teamNr].groups[groupNr].firstLED = number;
+					case 2:
+								LEDToTeam[number].teamNr = teamNr;
+								LEDToTeam[number].GroupNr= groupNr;
+								LEDToTeam[number].LEDNr= j - 1;
+								break;
+				}
+			}
+		}
 		return 0;
 	}else{
 		return 1;
@@ -69,6 +151,15 @@ uint8_t gamemode_start(uint8_t gameMode){
 }
 
 void gamemode_update(){
+	uint8_t i;
+	for (i = 0; i < LEDBOX_COUNT_MAX; i++) {
+		if (buttons_get(i)) {
+			pushButton(i);
+		}
+	}
+	//code to update RGB and IR LED
+	setLEDs();
+	showPoints(team[team1].points, team[team2].points);
 }
 
 void gamemode_finalize(uint8_t count, uint8_t mode){
@@ -88,5 +179,52 @@ void gamemode_to_display(uint8_t gameMode, uint8_t const** displayOut1, uint8_t 
 		*displayOut1=numbers[gameMode / 10];
 		*displayOut2=numbers[gameMode % 10];
 		break;
+	}
+}
+
+//Game priate
+
+void setLEDs(void){
+	rgb_setAll(NOCOLOR);
+
+	uint8_t  teamNr;
+	for (teamNr=0;teamNr<2;teamNr++){
+		uint8_t i;
+		const enum eColor color = team[teamNr].teamColor;
+		for(i=0;i<3;i++){
+			const uint8_t status = team[teamNr].groups[i].status;
+			const uint8_t number = team[teamNr].groups[i].firstLED;
+			uint8_t num;
+			for(num=0;num<2;num++){
+				ir_set(number + num, status & (num + 1));
+				if(status & (num + 1)) rgb_set(number + num, color);
+			}
+		}
+	}
+}
+
+void pushButton(uint8_t number){
+	const uint8_t teamNr = LEDToTeam[number].teamNr;
+	const uint8_t GroupNr = LEDToTeam[number].GroupNr;
+	const uint8_t LEDNr = LEDToTeam[number].LEDNr;
+	if (teamNr>1) return;
+
+	if (team[teamNr].groups[GroupNr].status){
+		team[teamNr].trys++;
+		if (team[teamNr].groups[GroupNr].status == LEDNr + 1) team[teamNr].points += POINTS_PER_PRESS;
+		if (team[teamNr].trys>=5){
+			uint8_t i;
+			for(i=0;i<3;i++){
+				team[teamNr].groups[i].status = groupOff;
+			}
+			team[teamNr].offGroup = 4;
+		}
+
+		if (team[teamNr].offGroup < 3){
+			team[teamNr].groups[team[teamNr].offGroup].status = random() % 2 + 1;
+		}
+
+		team[teamNr].groups[GroupNr].status = groupOff;
+		team[teamNr].offGroup = GroupNr;
 	}
 }
